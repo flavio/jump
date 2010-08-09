888c7db149ccb371fd310fd895be24c59c8705c5
Flavio Castelli <flavio@castelli.name>
2010-08-09 22:44:27 +0200

    fixed some bugs, improved the completion and upgraded the license

--- lib/bookmarks.rb
+++ lib/bookmarks.rb
@@ -1,21 +1,22 @@
-#!/usr/bin/env ruby
-
-# Jump, a bookmarking system for the bash shell.
-# Copyright (c) 2010 Giuseppe Capizzi
-# mailto: g.capizzi@gmail.com
+# This file is part of the jump project
 #
-# This program is free software: you can redistribute it and/or modify
+# Copyright (C) 2010 Flavio Castelli <flavio@castelli.name>
+# Copyright (C) 2010 Giuseppe Capizzi <gcapizzi@gmail.com>
+#
+# kaveau is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
+# the Free Software Foundation; either version 2 of the License, or
 # (at your option) any later version.
 #
-# This program is distributed in the hope that it will be useful,
+# kaveau is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# along with Keep; if not, write to the
+# Free Software Foundation, Inc.,
+# 51 Franklin Steet, Fifth Floor, Boston, MA 02110-1301, USA.
 
 require 'rubygems'
 require 'yaml'
@@ -73,8 +74,8 @@ class Bookmarks
     else
       bookmarks_table = table do |t|
         t.headings = "Bookmark", "Path"
-        @bookmarks.each do |bookmark, path|
-          t << [bookmark, path]
+        @bookmarks.keys.sort.each do |bookmark|
+          t << [bookmark, @bookmarks[bookmark]]
         end
       end
       bookmarks_table.to_s
@@ -83,11 +84,19 @@ class Bookmarks
 
   def bash_completion text
     if text.nil? || text.empty?
+      # nothing is provided -> return all the bookmarks
       @bookmarks.keys.sort.join(' ')
     elsif text.include? '/'
+      if text.index('/') == 0
+        # this is an absolute path (eg: /foo)
+        return text
+      end
+
+      # [bookmark]/path
       bookmark = text[0, text.index('/')]
       path = text[text.index('/')+1, text.size]
       if @bookmarks.has_key?(bookmark)
+        # this is a known bookmark
         entries = []
         Dir.foreach(@bookmarks[bookmark]) do |filename|
           next if !path.empty? && (filename =~ /\A#{path}.*/).nil?
@@ -96,6 +105,7 @@ class Bookmarks
             entries << File.join(bookmark, filename)
           end
         end
+
         if entries.empty?
           text
         else
@@ -103,9 +113,11 @@ class Bookmarks
           entries.sort.join(' ')
         end
       else
+        # this is an unknown bookmark
         text
       end
     else
+      # text could match one of the bookmarks
       matches = @bookmarks.keys.find_all { |b| b =~ /\A#{text}/ }
       if matches.empty?
         text
@@ -125,7 +137,9 @@ class Bookmarks
       return path_with_bookmark
     else
       # the path is composed of a bookmark and a subpath
-      name, path = path_with_bookmark.split("/")
+      name = path_with_bookmark[0, path_with_bookmark.index('/')]
+      path = path_with_bookmark[path_with_bookmark.index('/')+1,
+                                path_with_bookmark.size]
       return @bookmarks[name] + "/" + path;
     end
   end
--- test/bookmarks_test.rb
+++ test/bookmarks_test.rb
@@ -1,11 +1,32 @@
+# This file is part of the jump project
+#
+# Copyright (C) 2010 Flavio Castelli <flavio@castelli.name>
+# Copyright (C) 2010 Giuseppe Capizzi <gcapizzi@gmail.com>
+#
+# kaveau is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# kaveau is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Keep; if not, write to the
+# Free Software Foundation, Inc.,
+# 51 Franklin Steet, Fifth Floor, Boston, MA 02110-1301, USA.
+
 require "test_helper"
 require 'bookmarks'
 require 'fileutils'
 
 class BookmarksTest < Test::Unit::TestCase
   def setup
-    @test_bookmarks = {"foo" => "/tmp/foo",
-                  "bar" => "/tm[/bar"}
+    @test_bookmarks = { "foo" => "/tmp/foo",
+                        "bar" => "/tmp/bar",
+                        "complex" => "/tmp/foo/bar/complex"}
     FakeFS do
       File.open(Bookmarks::BOOKMARKS_PATH, 'w') do |file|
         file << YAML::dump(@test_bookmarks)
@@ -21,7 +42,12 @@ class BookmarksTest < Test::Unit::TestCase
       current_path = @bookmarks.expand_path bookmark
       assert_equal expected_path, current_path
     end
-    #TODO: cover other cases
+
+    # check absolute path
+    assert_equal "/foo", @bookmarks.expand_path("/foo")
+
+    # check multiple / handling
+    assert_equal "/tmp/foo/1/2", @bookmarks.expand_path("foo/1/2")
   end
 
   def test_add
@@ -39,12 +65,13 @@ class BookmarksTest < Test::Unit::TestCase
 
   def test_to_s
     output = <<EOF
-+----------+----------+
-| Bookmark | Path     |
-+----------+----------+
-| foo      | /tmp/foo |
-| bar      | /tm[/bar |
-+----------+----------+
++----------+----------------------+
+| Bookmark | Path                 |
++----------+----------------------+
+| bar      | /tmp/bar             |
+| complex  | /tmp/foo/bar/complex |
+| foo      | /tmp/foo             |
++----------+----------------------+
 EOF
     assert_equal output, @bookmarks.to_s
   end
@@ -80,6 +107,9 @@ EOF
       FileUtils.mkdir_p "/home/flavio/test/rails_app/app/model"
       FileUtils.touch "/home/flavio/test/rails_app/local_file"
 
+      # should handle absolute paths
+      assert_equal "/rails", bookmarks.bash_completion('/rails')
+
       # should return all the bookmarks
       assert_equal "rails templates test", bookmarks.bash_completion(nil)
       assert_equal "rails templates test", bookmarks.bash_completion('')
@@ -93,6 +123,8 @@ EOF
 
       # /home/flavio/templates/bar doesn't exist => should return the same text
       assert_equal "templates/bar", bookmarks.bash_completion("templates/bar")
+      assert_equal( "templates/bar/1/2",
+                    bookmarks.bash_completion("templates/bar/1/2"))
 
       # should expand the path
       assert_equal  "rails/ rails/locale rails/log",
